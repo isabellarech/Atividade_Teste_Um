1a8af46102ffdb7ff1af36073cfa8c98bbbb4445
Isabella <isabella_rech@estudante.sesisenai.org.br>
2026-05-21 12:02:37 -0300

    ADD - Criei a página de login com formulário de autenticação

---
+++ index.php
@@ -0,0 +1,27 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login com o Banco</title>
+</head>
+<body>
+    <h2>
+        Login com PHP
+    </h2>
+
+    <form method="POST">
+        <label for="usuario">Usuário:</label>
+        <input type="text" id="usuario" name="usuario">
+
+        <br></br>
+
+        <label for="senha" class="senha">Senha:</label>
+        <input type="password" id="senha" name="senha">
+
+        <br></br>
+
+        <button type="submit">Entrar</button>
+    </form>
+
+</body>
+</html>
