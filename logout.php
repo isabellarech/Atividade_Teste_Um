7b060f2c2754ea43cbda4b110808cb48a2703365
Isabella <isabella_rech@estudante.sesisenai.org.br>
2026-06-02 10:58:17 -0300

    ADD - Atualizei os comentários no arquivo logout.php

--- logout.php
+++ logout.php
@@ -1,8 +1,11 @@
 <?php
+// Essa página serve para encerrar a sessão do usuário
 session_start();
+// Encerra a sessão do usuário, removendo todas as variáveis de sessão e destruindo a sessão
 session_destroy();
-
+// Redireciona o usuário para a página de login após encerrar a sessão
 header("Location:index.php");
+// Encerra a execução do script para garantir que o redirecionamento ocorra corretamente
 exit();
 
 ?>
