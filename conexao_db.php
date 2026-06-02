5d4c799be676d73c97c720e31e87c15e9bce2206
Isabella <isabella_rech@estudante.sesisenai.org.br>
2026-06-02 09:44:41 -0300

    ADD - Adicionei comentários sobre a conexão com o banco de dados

--- conexao_db.php
+++ conexao_db.php
@@ -1,4 +1,7 @@
 <?php
+    // Essa parte conecta o banco de dados, e é usada em todas as páginas que precisam acessar o banco
+
+    // Primeiro, definimos as variáveis de conexão com o banco de dados
 
     $host = "localhost";
     $user = "root";
